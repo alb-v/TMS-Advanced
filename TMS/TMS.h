7cde7d2d754b5d82fbc3507589676e1ebae6894e
albvol <volpea.93@gmail.com>
2017-09-09 12:17:32 +0200

    Initial Commit

---
+++ TMS/TMS.h
@@ -0,0 +1,19 @@
+//
+//  TMS.h
+//  TMS
+//
+//  Created by Alberto Volpe on 09/09/17.
+//  Copyright © 2017 Alberto Volpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMS.
+FOUNDATION_EXPORT double TMSVersionNumber;
+
+//! Project version string for TMS.
+FOUNDATION_EXPORT const unsigned char TMSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMS/PublicHeader.h>
+
+
